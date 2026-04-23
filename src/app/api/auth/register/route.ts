924c68469e12e2a801175911d270eec76398a80a
Shrawan2002 <shrawankumar1754@gmail.com>
2026-04-23 12:16:50 +0530

    first commit

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,29 @@
+import User from "@/app/models/user.model";
+import connectDb from "@/lib/db";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export async function POST(req:NextRequest){
+    try{
+        const {name,email,password} = await req.json()
+        await connectDb();
+        let user = await User.findOne({email})
+        if(user){
+            return NextResponse.json(
+                {message: "email already exist!"},
+                {status:400}
+            )
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user = await User.create({name,email,password:hashedPassword})
+        return NextResponse.json(user, {status:201})
+        
+    }catch(error){
+        return NextResponse.json(
+            {message: `register Error ${error}`},
+            {status:500}
+        )
+    }
+}
